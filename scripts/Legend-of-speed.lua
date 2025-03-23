3966f82de543347d2f6b678be47d717f7f15c0c8
MarkHildegard <mark.krause09@hotmail.com>
2025-03-23 13:29:45 +0100

    Update Legend-of-speed.lua

--- scripts/Legend-of-speed.lua
+++ scripts/Legend-of-speed.lua
@@ -14,7 +14,7 @@ local FarmTab = Window:CreateTab("💫 Auto Orb Farm", 4483362458)
 local orbFarm = {
     ["City"] = false,
     ["Snow City"] = false,
-    ["Lava City"] = false
+    ["Magma City"] = false
 }
 
 -- Farming Loop für Orbs
@@ -53,7 +53,7 @@ end
 
 createOrbToggle("City")
 createOrbToggle("Snow City")
-createOrbToggle("Lava City")
+createOrbToggle("Magma City")
 
 -- TAB: Misc
 local MiscTab = Window:CreateTab("⚙️ Misc", 4483362458)
@@ -77,8 +77,64 @@ task.spawn(function()
             local args = {
                 [1] = "rebirthRequest"
             }
-
+            -- Schickt die Rebirth-Anfrage an den Server
             game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("rebirthRemote"):FireServer(unpack(args))
         end
     end
 end)
+
+-- TAB: Teleport zu verschiedenen Welten
+local TeleportTab = Window:CreateTab("🌍 Teleport", 4483362458)
+
+-- Funktion zum Erstellen der Teleport-Tasten
+local function createTeleportButton(worldName, position)
+    TeleportTab:CreateButton({
+        Name = "Teleport to " .. worldName,
+        Callback = function()
+            if position then
+                game:GetService("Players").LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(position))
+            else
+                warn("Teleport location for " .. worldName .. " not found.")
+            end
+        end,
+    })
+end
+
+-- Positionsangaben für die Teleports
+local teleportLocations = {
+    City = workspace.areaCircles:GetChildren()[2] and workspace.areaCircles:GetChildren()[2].Position, -- City Position
+    ["Snow City"] = workspace.areaCircles:GetChildren()[16] and workspace.areaCircles:GetChildren()[16].Position, -- Snow City Position
+    ["Magma City"] = workspace.areaCircles:GetChildren()[25] and workspace.areaCircles:GetChildren()[25].Position -- Magma City Position
+}
+
+-- Erstellen der Teleport-Buttons für jede Welt
+createTeleportButton("City", teleportLocations.City)
+createTeleportButton("Snow City", teleportLocations["Snow City"])
+createTeleportButton("Magma City", teleportLocations["Magma City"])
+
+-- Manuelles Sammeln von Orbs für Magma City
+local function collectMagmaCityOrbs()
+    local orbs = {
+        {"Red Orb", "Magma City"},
+        {"Yellow Orb", "Magma City"},
+        {"Blue Orb", "Magma City"},
+        {"Gem", "Magma City"}
+    }
+    
+    for _, orb in pairs(orbs) do
+        local args = {
+            [1] = "collectOrb",
+            [2] = orb[1],
+            [3] = orb[2]
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+    end
+end
+
+-- Magma City Orb Farming
+FarmTab:CreateButton({
+    Name = "Collect Magma City Orbs",
+    Callback = function()
+        collectMagmaCityOrbs()
+    end,
+})
